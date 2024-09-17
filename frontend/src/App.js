335216d1d54e536ceff07480e44dc918401a6c95
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2024-09-17 23:41:42 +0300

    start project

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/data/')
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the data!', error);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Data from Django API</h1>
+      <ul>
+        {data.map(item => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
