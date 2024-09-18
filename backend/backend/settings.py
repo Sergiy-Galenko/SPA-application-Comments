c05512716812b98355fd093eba70b3a1db5320a8
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2024-09-18 10:52:07 +0300

    Implement user registration with custom User model (username and email validation)

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
 ]
+
+AUTH_USER_MODEL = 'users.CustomUser'
